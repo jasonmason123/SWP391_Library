bef6caf78e4b0cf99e9f80b6a2d0f76f651bfea2
jasonmason123 <buiminhson940@gmail.com>
2024-05-23 10:16:30 +0700

    fix bug

--- Library/src/main/java/com/springdemo/library/repositories/BinhLuanBlogRepository.java
+++ Library/src/main/java/com/springdemo/library/repositories/BinhLuanBlogRepository.java
@@ -1,8 +1,9 @@
 package com.springdemo.library.repositories;
 
+import com.springdemo.library.model.BinhLuanBlog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BinhLuanBlog extends JpaRepository<BinhLuanBlog, Integer> {
+public interface BinhLuanBlogRepository extends JpaRepository<BinhLuanBlog, Integer> {
 }
