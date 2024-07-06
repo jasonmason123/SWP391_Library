c9973b5dfd37d002512f1c884ed83d7a0e22f7cb
tumin11 <hovutumin11042004@gmail.com>
2024-07-06 12:30:39 +0700

    sachRepository

--- Library/src/main/java/com/springdemo/library/repositories/SachRepository.java
+++ Library/src/main/java/com/springdemo/library/repositories/SachRepository.java
@@ -1,17 +1,35 @@
 package com.springdemo.library.repositories;
-
-import com.springdemo.library.model.DanhMuc;
 import com.springdemo.library.model.Sach;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SachRepository extends JpaRepository<Sach, Integer> {
+    @Query(value = "SELECT TOP 7 s.nhaXuatBan FROM Sach s GROUP BY s.nhaXuatBan", nativeQuery = true)
+    List<String> findTopNhaXuatBan();
+
+    @Query(value = """
+            SELECT TOP 7 s.nhaXuatBan FROM Sach s JOIN TagTheLoai t ON s.Id = t.SachId
+            WHERE t.TheLoaiId = :theLoaiId GROUP BY s.nhaXuatBan""", nativeQuery = true)
+    List<String> findTopNhaXuatBanByTheLoaiId(@Param("theLoaiId") int theLoaiId);
+
+    @Query(value = "SELECT TOP 7 s.tacGia FROM Sach s GROUP BY s.tacGia", nativeQuery = true)
+    List<String> findTopTacGia();
+
+    @Query(value = """
+            SELECT TOP 7 s.tacGia FROM Sach s JOIN TagTheLoai t ON s.Id = t.SachId
+            WHERE t.TheLoaiId = :theLoaiId GROUP BY s.tacGia""", nativeQuery = true)
+    List<String> findTopTacGiaByTheLoai(@Param("theLoaiId") int theLoaiId);
+
+    @Query(value = "SELECT DISTINCT TOP 5 s.* FROM Sach s JOIN TagTheLoai t ON s.Id = t.SachId WHERE t.TheLoaiId IN :theLoaiIdList", nativeQuery = true)
+    List<Sach> findSachByListOfTheLoai(@Param("theLoaiIdList") List<Integer> theLoaiIdList);
+
+    @Query("SELECT s FROM Sach s JOIN s.theLoaiList t WHERE t.Id=:theLoaiId")
+    List<Sach> findSachByTheLoaiId(@Param("theLoaiId") int theLoaiId);
     @Query(value = "select Sach.Id,Sach.tenSach,Sach.moTa,Sach.giaTien,Sach.danhGia,Sach.soLuongTrongKho,Sach.linkAnh,Sach.tacGia,Sach.nhaXuatBan,Sach.flagDel,Sach.dateCreated,Sach.dateUpdated \n" +
             "from Sach join TagTheLoai on Sach.Id=TagTheLoai.SachId join TheLoai on TagTheLoai.TheLoaiId=TheLoai.Id join DanhMuc on TheLoai.DanhMucId=DanhMuc.Id\n" +
             "where DanhMuc.tenDanhMuc=:tenDanhMuc",nativeQuery = true)
